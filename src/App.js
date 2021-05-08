602f9c568faf33824fc61086bb33b1cecd4dbccd
Jemiydaud <jemilaydaud@gmail.com>
2021-05-08 17:30:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from  'axios'; 
 import './App.css';
+import Ip from './components/IP/IP.js';
+import Map from './components/Map/Map.js';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [ipAddress, setIpAddress] = useState([])
+  //const [isLoading, setLoading] = useState(true)
+  const [lat,setLat] = useState([])
+  const [lng,setLng] = useState([])
 
-export default App;
+ const  API_KEY = process.env.REACT_APP_IPFY_API_KEY ;
+    console.log(API_KEY)
+  
+
+  
+  useEffect(() => {
+    async function fetchIp() {
+      try {
+           axios.get(`https://geo.ipify.org/api/v1?apiKey=${API_KEY}
+           `).then((result) => {
+            console.log(result.data)
+            const userIp = result.data.ip;
+            const userLng = result.data.location.lng;
+            const userLat = result.data.location.lat;
+            setIpAddress(userIp);
+            setLat(result.data.location.lat);
+            console.log(lat)
+            setLng(result.data.location.lng);
+            console.log(lng);
+            //setIsLoading(false);
+            console.log(userLng);
+            console.log(userLat);
+          });
+        } catch (error) {
+          alert("No results");
+        }
+      }
+    
+      fetchIp();
+    
+    },[lng, lat, ipAddress]);
+    
+    
+    return (
+      <div className="App">
+    
+        <h1>
+          Whats My IP Address
+        </h1>
+    
+        <Ip ipAddress={ipAddress}  />
+        <Map lat={lat} lng={lng} />
+    
+    
+    
+      </div>
+    );
+  };
+  
+  export default App;
